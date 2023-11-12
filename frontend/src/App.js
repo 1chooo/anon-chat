7ad65f22eb2da534c6ab48420252841add8080ae
1chooo <hugo970217@gmail.com>
2023-11-12 14:12:37 +0800

    add: ChatHistory components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,24 @@ import React, { Component } from "react";
 import "./App.css";
 import { connect, sendMsg } from "./api";
 import Header from './components/Header/Header';
+import ChatHistory from './components/ChatHistory/ChatHistory';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
-		connect();
+		this.state = {
+			chatHistory: []
+		}
+	}
+
+	componentDidMount() {
+		connect((msg) => {
+			console.log("New Message")
+			this.setState(prevState => ({
+				chatHistory: [...this.state.chatHistory, msg]
+			}))
+			console.log(this.state);
+		});
 	}
 
 	send() {
@@ -16,10 +29,11 @@ class App extends Component {
 
 	render() {
 		return (
-		<div className="App">
-			<Header />
-			<button onClick={this.send}>Hit</button>
-		</div>
+			<div className="App">
+				<Header />
+				<ChatHistory chatHistory={this.state.chatHistory} />
+				<button onClick={this.send}>Hit</button>
+			</div>
 		);
 	}
 }
--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,8 +1,8 @@
 // api/index.js
 var socket = new WebSocket("ws://localhost:8080/ws");
 
-let connect = () => {
-	console.log("Attempting Connection...");
+let connect = cb => {
+	console.log("connecting");
 
 	socket.onopen = () => {
 		console.log("Successfully Connected");
@@ -10,6 +10,7 @@ let connect = () => {
 
 	socket.onmessage = msg => {
 		console.log(msg);
+		cb(msg);
 	};
 
 	socket.onclose = event => {
@@ -20,6 +21,8 @@ let connect = () => {
 		console.log("Socket Error: ", error);
 	};
 };
+
+
 
 let sendMsg = msg => {
 	console.log("sending msg: ", msg);

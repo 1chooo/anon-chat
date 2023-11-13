d7d319e6802590225044320733783bd7a6d6fc9c
1chooo <hugo970217@gmail.com>
2023-11-13 15:51:21 +0800

    update: key-on

--- frontend/src/components/ChatInput/ChatInput.jsx
+++ frontend/src/components/ChatInput/ChatInput.jsx
@@ -2,10 +2,18 @@ import React, { Component } from "react";
 import "./ChatInput.scss";
 
 class ChatInput extends Component {
+    handleKeyDown = (e) => {
+        // 檢查按下的鍵是否是 Enter
+        if (e.key === 'Enter') {
+            // 調用 send 函數
+            this.props.send();
+        }
+    };
+
     render() {
         return (
             <div className="ChatInput">
-                <input onKeyDown={this.props.send} />
+                <input onKeyDown={this.handleKeyDown} />
             </div>
         );
     }
